9bd30d12320f5b62f7c1d229fb22fa3dd3f17dce
Ethan <ethan@fasetto.com>
2017-10-02 14:44:31 -0500

    Promise: setup promise object for Promise<void> return values

--- Bridge.AspNetCore.SignalR.Client/Threading/Promise.cs
+++ Bridge.AspNetCore.SignalR.Client/Threading/Promise.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 namespace Bridge.AspNetCore.SignalR.Client.Threading
 {
     /// <summary>
-    /// Javascript Promise with a result 
+    /// A promise of value from the completion of an operation
     /// </summary>
     /// <typeparam name="TResult">The type of the expected result</typeparam>
     [External]
@@ -12,7 +12,7 @@ namespace Bridge.AspNetCore.SignalR.Client.Threading
     [Convention(Target = ConventionTarget.Member, Notation = Notation.LowerCamelCase)]
     public class Promise<TResult> : IPromise
     {
-        #region Public Methods
+        #region IPromise Implementation
 
         /// <summary>
         /// Adds a fulfilledHandler, errorHandler to be called for completion of a promise.
@@ -22,6 +22,27 @@ namespace Bridge.AspNetCore.SignalR.Client.Threading
         /// <param name="progressHandler">The progressHandler is called when the promise has made progress.</param>
         public void Then(Delegate fulfilledHandler, Delegate errorHandler = null, Delegate progressHandler = null) { }
      
+        #endregion
+    }
+
+    /// <summary>
+    /// A promise of value from the completion of an operation
+    /// </summary>
+    [External]
+    [Namespace(false)]
+    [Convention(Target = ConventionTarget.Member, Notation = Notation.LowerCamelCase)]
+    public class Promise : IPromise
+    {
+        #region IPromise Implementation
+
+        /// <summary>
+        /// Adds a fulfilledHandler, errorHandler to be called for completion of a promise.
+        /// </summary>
+        /// <param name="fulfilledHandler">The fulfilledHandler is called when the promise is fulfilled.</param>
+        /// <param name="errorHandler">The errorHandler is called when a promise fails.</param>
+        /// <param name="progressHandler">The progressHandler is called when the promise has made progress.</param>
+        public void Then(Delegate fulfilledHandler, Delegate errorHandler = null, Delegate progressHandler = null) { }
+
         #endregion
     }
 }
